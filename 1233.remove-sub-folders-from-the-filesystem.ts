7d88dbb81ab21e0a3a2b1d0cda4a78575fb64139
CaoHongHai <caohonghai@caohonghai.com>
2023-02-08 20:44:34 +0800

    chore: 1233 tire tree

--- 1233.remove-sub-folders-from-the-filesystem.ts
+++ 1233.remove-sub-folders-from-the-filesystem.ts
@@ -21,4 +21,63 @@ function removeSubfolders(folder: string[]): string[] {
     }
     return res;
 }
+class TrieNode {
+    private map: Map<string, TrieNode>;
+
+    constructor(public ref: number = -1) {
+        this.map = new Map<string, TrieNode>();
+    }
+
+    get(key: string) {
+        return this.map.get(key);
+    }
+
+    set(key: string, value: TrieNode) {
+        return this.map.set(key, value);
+    }
+
+    getChildren() {
+        return Array.from(this.map.values());
+    }
+}
+
+/**
+ * 字典树做法
+ * @param folder
+ * @returns
+ */
+function removeSubfolders_2(folder: string[]): string[] {
+    const root = new TrieNode();
+
+    const n = folder.length;
+    for (let i = 0; i < n; i++) {
+        const path = folder[i].slice(1).split('/');
+        let cur = root;
+        for (const name of path) {
+            let node = cur.get(name);
+            if (node === undefined) {
+                cur.set(name, (node = new TrieNode()));
+            }
+            cur = node;
+        }
+        cur.ref = i;
+    }
+
+    const ans: string[] = [];
+    const dfs = (node: TrieNode) => {
+        if (node.ref !== -1) {
+            ans.push(folder[node.ref]);
+            return;
+        }
+
+        const children = node.getChildren();
+        if (children.length) {
+            for (const child of children) {
+                dfs(child);
+            }
+        }
+    };
+
+    return dfs(root), ans;
+}
 // @lc code=end
